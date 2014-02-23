a5d8ad0c52e02c3a5d76203c0669cfa85f9fd367
thomaskkk <thomas.adm@gmail.com>
2014-02-23 10:38:43 -0300

    Initial Commit

--- server_config/mysql_remote_permissions/permissions.sql
+++ server_config/mysql_remote_permissions/permissions.sql
@@ -8,3 +8,4 @@ UPDATE mysql.user SET Password=PASSWORD('batatafrita') WHERE User='root';
 -- Allow root access from 192.168.50.1
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.50.1'IDENTIFIED BY 'batatafrita' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
